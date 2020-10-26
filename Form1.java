8955bbdfd633de323d5de49aaa4980eb5db1a31d
Kumarsaurabh14 <43724386+Kumarsaurabh14@users.noreply.github.com>
2020-10-26 09:04:14 +0530

    Commit 3
    Now we can add data into table using GUI. 
    Modified GUL layout (from flow to fixed).

--- Form1.java
+++ Form1.java
@@ -32,22 +32,30 @@ public class Form1 extends JFrame implements ActionListener
 		nameLb=new JLabel("Name");
 		rollnoLb=new JLabel("Roll NO:");
 		stateLb=new JLabel("State");
-
+		
 		nameTf=new JTextField(20);
 		rollnoTf=new JTextField(20);
 		stateTf=new JTextField(20);
-
+		
 		submitBt=new JButton("submit");
 		submitBt.addActionListener(this);
-		setLayout(new FlowLayout());
+		setLayout(null);
+		//setLayout(new FlowLayout());
+		nameLb.setBounds(10,10,50,20);
 		add(nameLb);
+		nameTf.setBounds(70,10,50,20);
 		add(nameTf);
+		rollnoLb.setBounds(10,40,50,20);
 		add(rollnoLb);
+		rollnoTf.setBounds(70,40,50,20);
 		add(rollnoTf);
+		stateLb.setBounds(10,70, 50, 20);
 		add(stateLb);
+		stateTf.setBounds(70,70,50,20);
 		add(stateTf);
+		submitBt.setBounds(10,100,100,20);
 		add(submitBt);
-
+		
 		
 	}
 	
@@ -55,13 +63,23 @@ public class Form1 extends JFrame implements ActionListener
 	{
 		if(e.getSource()==submitBt)
 		{
-			String name = nameTf.getText();
-			String rollno = rollnoTf.getText();
-			String state = stateTf.getText();
-			System.out.println("Button has been pressed.");
-			System.out.println("Name: "+name);
-			System.out.println("RollNo.: "+rollno);
-			System.out.println("State: "+state);
+			String name=nameTf.getText();
+			String rollno=rollnoTf.getText();
+			String state=stateTf.getText();
+			System.out.println("button has been pressed");
+			System.out.println("Name :"+name);
+			System.out.println("RollNo :"+rollno);
+			System.out.println("State  :"+state);
+			
+			String query="insert into stu_table(name, rollno, state) values('"+name+"','"+rollno+"','"+state+"');";
+			try
+			{
+				smt.executeUpdate(query);
+			}
+			catch(Exception e1)
+			{
+			 	System.out.println(e1);
+			}
 		}
 	}
 	public static void main(String args[])
